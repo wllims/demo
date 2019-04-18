79d31306470954e5795f77f37249b55d5d0d1be1
root <1766905089@qq.com>
2019-04-18 08:49:27 +0800

    cmfz

---
+++ cmfz_hjd/src/main/java/com/baizhi/dao/MenuDao.java
@@ -0,0 +1,10 @@
+package com.baizhi.dao;
+
+import com.baizhi.entity.Menu;
+import tk.mybatis.mapper.common.Mapper;
+
+import java.util.List;
+
+public interface MenuDao extends Mapper<Menu> {
+    List<Menu> selectAllMenu();
+}
